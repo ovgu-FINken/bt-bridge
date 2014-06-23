389ab02a8558a780c833872538a1ceab4813bed7
Sebastian Mai <bastinat0r@bastinat0r.de>
2014-06-23 21:47:09 +0200

    usability for bt-script
    * paparazzi-link
    * bt-mac as parameter

--- mkpty.sh
+++ mkpty.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-socat -d -d pty EXEC:'./bt-bridge 88\:33\:14\:E5\:B3\:0C 0x12'
+socat -d -d pty EXEC:"./bt-bridge $1 0x12"
+SOCAT_PID=$!
+echo $SOCAT_PID
+PTS_NAME=$(ls -la /proc/$SOCAT_PID/fd | grep -oe '/dev/pts/[1-9][0-9]*')
+echo $PTS_NAME
+#$PAPARAZZI_HOME/sw/ground/segment/tmtc/link -d $PTS_NAME
